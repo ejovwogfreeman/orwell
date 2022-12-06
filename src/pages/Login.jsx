0fe95b607a7d8651dd7d1cf535162aa451ebdc28
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-12-06 23:54:20 +0100

    message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Login.css";
 import ScrollToTop from "../components/ScrollToTop";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div>
+    <>
       <ScrollToTop />
-    </div>
+      <header style={{ height: "400px" }}>
+        <p className="display-3">Login</p>
+      </header>
+      <div>
+        <div className="login-cont">
+          <form onSubmit={handleSubmit}>
+            <h4 className="mb-3">LOGIN HERE</h4>
+            <div>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="YOUR EMAIL*"
+                required
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="YOUR NAME*"
+                required
+              />
+            </div>
+            <button>LOGIN</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ScrollToTop from "../components/ScrollToTop";
+// import img from "../assets/"
+
+// const projects = [
+//     {
+
+//     }
+// ]
 
 const Projects = () => {
   return (
     <div>
       <ScrollToTop />
+      <div></div>
     </div>
   );
 };
