1de91c28003ed79ec0114a5480f0222f36a7e291
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-12-07 01:16:26 +0100

    message

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,10 +1,61 @@
 import React from "react";
 import ScrollToTop from "../components/ScrollToTop";
+import img from "../assets/house1.jpg";
+import "../css/People.css";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const People = () => {
   return (
-    <div>
+    <div className="people-cont">
       <ScrollToTop />
+      <p className="display-5 text-center pb-3">People</p>
+      <div className="people">
+        <div className="person">
+          <img src={img} alt="" style={{ width: "200px" }} />
+          <h3 className="mt-3">Neil Strelitz</h3>
+          <h5>Director</h5>
+          <h5>MRICS</h5>
+          <div className="icons">
+            <a href="https://facebook.com">
+              <FaFacebookSquare />
+            </a>
+            <a href="https://twitter.com">
+              <FaTwitterSquare />
+            </a>
+            <a href="https://instagram.com">
+              <FaInstagramSquare />
+            </a>
+            <a href="https://linkedin.com">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+        <div className="person">
+          <img src={img} alt="" style={{ width: "200px" }} />
+          <h3 className="mt-3">Neil Strelitz</h3>
+          <h5>Director</h5>
+          <h5>MRICS</h5>
+          <div className="icons">
+            <a href="https://facebook.com">
+              <FaFacebookSquare />
+            </a>
+            <a href="https://twitter.com">
+              <FaTwitterSquare />
+            </a>
+            <a href="https://instagram.com">
+              <FaInstagramSquare />
+            </a>
+            <a href="https://linkedin.com">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,53 @@
 import React from "react";
+import "../css/Services.css";
 import ScrollToTop from "../components/ScrollToTop";
 
 const Services = () => {
   return (
-    <div>
+    <>
       <ScrollToTop />
-    </div>
+      <p className="display-5 text-center pt-5">Services</p>
+      <div className="service-text">
+        Thank you for trusting us with the search for your new investment. We're
+        here to help you in your hunt to find the right investment for your
+        family. <br /> Orwell Properties Management Limited is one of the
+        leading real estate company in Nigeria that aims to providing unique
+      </div>
+      <header
+        style={{
+          height: "200px",
+          justifyContent: "left",
+          padding: "50px 5%",
+        }}
+      >
+        <div>
+          Thank you for trusting us with the search for your new investment.
+          We're here to help you in your hunt to find the right investment for
+          your family. <br /> Orwell Properties Management Limited is one of the
+          leading real estate company in Nigeria that aims to providing unique
+        </div>
+      </header>
+      <div className="service-text">
+        Thank you for trusting us with the search for your new investment. We're
+        here to help you in your hunt to find the right investment for your
+        family. <br /> Orwell Properties Management Limited is one of the
+        leading real estate company in Nigeria that aims to providing unique
+      </div>
+      <header
+        style={{
+          height: "200px",
+          justifyContent: "left",
+          padding: "50px 5%",
+        }}
+      >
+        <div>
+          Thank you for trusting us with the search for your new investment.
+          We're here to help you in your hunt to find the right investment for
+          your family. <br /> Orwell Properties Management Limited is one of the
+          leading real estate company in Nigeria that aims to providing unique
+        </div>
+      </header>
+    </>
   );
 };
 
