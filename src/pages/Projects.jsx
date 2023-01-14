30403d1744f2910eb4c93b5c7300f6a2eef9e9b1
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-01-14 15:08:05 +0100

    message

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ScrollToTop from "../components/ScrollToTop";
-import img1 from "../assets/house1.jpg";
-import img2 from "../assets/house2.jpg";
-import img3 from "../assets/house3.jpg";
+import img1 from "../assets/cedar_court_1.jpg";
+import img2 from "../assets/cedar_court_2.jpg";
+import img3 from "../assets/cedar_court_3.jpg";
+import img4 from "../assets/frazer_1.jpg";
+import img5 from "../assets/frazer_2.jpg";
+import img6 from "../assets/frazer_3.jpg";
+import img7 from "../assets/juniper_court_1.jpg";
+import img8 from "../assets/juniper_court_2.jpg";
+import img9 from "../assets/juniper_court_3.jpg";
+import img10 from "../assets/magnolia_court_1.jpg";
+import img11 from "../assets/magnolia_court_2.jpg";
+import img12 from "../assets/magnolia_court_3.jpg";
+import img13 from "../assets/medina_court_1.jpg";
+import img14 from "../assets/medina_court_2.jpg";
+import img15 from "../assets/medina_court_3.jpg";
 import "../css/Projects.css";
 import { Link } from "react-router-dom";
 
@@ -10,47 +22,77 @@ const projects = [
   {
     id: 1,
     img: img1,
-    text: "Bentford city",
+    text: "Cedar Court",
   },
   {
     id: 2,
     img: img2,
-    text: "Bentford city",
+    text: "Cedar Court",
   },
   {
     id: 3,
     img: img3,
-    text: "Bentford city",
+    text: "Cedar Court",
   },
   {
     id: 4,
-    img: img2,
-    text: "Bentford city",
+    img: img4,
+    text: "Frazer",
   },
   {
     id: 5,
-    img: img3,
-    text: "Bentford city",
+    img: img5,
+    text: "Frazer",
   },
   {
     id: 6,
-    img: img1,
-    text: "Bentford city",
+    img: img6,
+    text: "Frazer",
   },
   {
     id: 7,
-    img: img3,
-    text: "Bentford city",
+    img: img7,
+    text: "Juniper Court",
   },
   {
     id: 8,
-    img: img1,
-    text: "Bentford city",
+    img: img8,
+    text: "Juniper Court",
   },
   {
     id: 9,
-    img: img2,
-    text: "Bentford city",
+    img: img9,
+    text: "Juniper Court",
+  },
+  {
+    id: 10,
+    img: img10,
+    text: "Magnolia Court",
+  },
+  {
+    id: 11,
+    img: img11,
+    text: "Magnolia Court",
+  },
+  {
+    id: 12,
+    img: img12,
+    text: "Magnolia Court",
+  },
+  {
+    id: 13,
+    img: img13,
+    text: "Medina Court",
+  },
+  {
+    id: 14,
+    img: img14,
+    text: "Medina Court",
+  },
+  {
+    id: 15,
+    img: img15,
+    text: "Medina Court",
   },
 ];
 
