50ec0c126ca922ce61b55e8f7bbb472416ce464e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-01-14 14:31:48 +0100

    message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../css/Footer.css";
 
 const Footer = () => {
-  return <footer>&copy; COPYRIGHT 2021 - ALL RIGHTS RESERVED.</footer>;
+  return (
+    <footer>
+      &copy; COPYRIGHT {new Date().getFullYear()} - ALL RIGHTS RESERVED.
+    </footer>
+  );
 };
 
 export default Footer;
